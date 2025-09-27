513099e39d864511313695247b03eae3e8589e50
Raytwo <raytwost@gmail.com>
2025-09-27 20:39:02 +0200

    Move code to a new folder in preparation for the dumper

---
+++ ProjectData~/Assets/Scripts/Assembly-CSharp/Bridge/TerrainPaintData.cs
@@ -0,0 +1,7 @@
+namespace Bridge
+{
+	public class TerrainPaintData : App.TerrainPaintData
+	{
+		//Dummy class. Use different settings or provide .NET dll files for better decompilation output
+	}
+}
