6751dd39dc8f9c7783b883573af0e2cdd9811ea5
Raytwo <raytwost@gmail.com>
2025-09-28 19:02:34 +0200

    Inline Volume class

--- ProjectData~/Assets/Scripts/Assembly-CSharp/App/PostProcessManager.cs
+++ ProjectData~/Assets/Scripts/Assembly-CSharp/App/PostProcessManager.cs
@@ -1,12 +1,27 @@
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
+
 
 namespace App
 {
 	[ExecuteInEditMode] 
 	public abstract class PostProcessManager : SingletonMonoBehaviour<PostProcessManager>
 	{
-		private Volume m_Root; // 0x20
+        // This class is originally from UnityEngine.Rendering,  but it blocks the template from opening properly when depended on.
+        // If PostProcessManager does not work as expected, this is the first thing to investigate.
+        public class Volume : MonoBehaviour
+        {
+            public bool isGlobal = true;
+
+            public float priority;
+
+            public float blendDistance;
+
+            [Range(0f, 1f)]
+            public float weight = 1f;
+        }
+
+        private Volume m_Root; // 0x20
 		private Volume m_Bmap; // 0x28
 		private Volume m_Combat; // 0x30
 		public float BmapCombatChangeTime; // 0x38
