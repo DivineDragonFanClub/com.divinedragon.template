219dae953c7c7462269393cfa471d5727b91eb91
Raytwo <raytwost@gmail.com>
2025-10-15 23:38:26 +0200

    Default values for WwiseObjectReference

--- ProjectData~/Assets/Scripts/Assembly-CSharp/WwiseObjectReference.cs
+++ ProjectData~/Assets/Scripts/Assembly-CSharp/WwiseObjectReference.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 
 public class WwiseObjectReference : ScriptableObject
 {
-	public string objectName;
-	public uint id;
-	public string guid;
-	public Guid Guid;
+	public string objectName = "EventName";
+	public uint id = 0;
+	public string guid = Guid.NewGuid().ToString();
+	public Guid Guid = new Guid();
 	private string ObjectName;
 	private string DisplayName;
 	private uint Id;
