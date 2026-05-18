387717f7d5a985e8c4564ec6b31a5fe06cf26ebd
BadAtGames26 <125292563+BadAtGames26@users.noreply.github.com>
2026-05-17 23:28:06 -0700

    Add Custom Inspector for MagicBridge and name enums for MagicSignal (#13)

--- ProjectData~/Assets/Scripts/Assembly-CSharp/Combat/MagicSignal.cs
+++ ProjectData~/Assets/Scripts/Assembly-CSharp/Combat/MagicSignal.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,31 +19,53 @@ namespace Combat
 
     public enum MagicCommand // TypeDefIndex: 8782
     {
+        [InspectorName("None")]
         なし = 0,
+        [InspectorName("Create Effect")]
         エフェ生成 = 1,
+        [InspectorName("Delete Effect")]
         エフェ削除 = 2,
+        [InspectorName("Fire Magic Bullet")]
         魔弾発射 = 3,
+        [InspectorName("Magic Bullet Impact")]
         魔弾衝突 = 4,
+        [InspectorName("Stop Magic Bullet")]
         魔弾停止 = 5,
+        [InspectorName("Change Target")]
         目標変更 = 6,
+        [InspectorName("Sound")]
         サウンド = 7,
+        [InspectorName("Switch Cut")]
         カット切替 = 8,
+        [InspectorName("Camera")]
         カメラ = 9,
+        [InspectorName("Restore Camera")]
         カメラ戻す = 10,
+        [InspectorName("Radial Blur")]
         ラジブラー = 11,
+        [InspectorName("Background Darkness")]
         背景暗さ = 12,
+        [InspectorName("Move Node")]
         ノード移動 = 13,
+        [InspectorName("Create New Effect")]
         新エフェ生成 = 14,
     }
 
     public enum ParticleConnect // TypeDefIndex: 8783
     {
+        [InspectorName("Unset")]
         未設定 = 0,
+        [InspectorName("Connect To")]
         に接続 = 1,
+        [InspectorName("Match Orientation")]
         の姿勢 = 2,
+        [InspectorName("Match Position")]
         の位置 = 3,
+        [InspectorName("Match Ground")]
         の地面 = 4,
+        [InspectorName("Tiki Claws")]
         チキ爪 = 5,
+        [InspectorName("Match Ground Horizontal")]
         の地面水平 = 6,
     }
 
