c6dae1d4e92a486e4c8d4354ebe7251a6157a056
Alexander Ren <alex.ren@gmail.com>
2026-06-13 22:49:35 -0700

    straight copy from animation tools

---
+++ ProjectData~/Assets/Scripts/Assembly-CSharp/PrefetchedCurve_Bridge.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Prefetched Curve", menuName = "Divine Dragon/Animation Tools/Create Prefetched Curve",
+    order = 1)]
+public class PrefetchedCurve_Bridge: PrefetchedCurve
+{
+    
+}
